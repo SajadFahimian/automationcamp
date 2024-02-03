773dcae468e0c09f73e3679a7b54144c86e25496
Sajad Fahimian <s.fahimian0040@gmail.com>
2024-02-03 07:26:41 +0330

    Session 9 end

--- Archive/main.py
+++ Archive/main.py
@@ -124,27 +124,89 @@ actions = ActionChains(driver)
 
 
 
-driver.get("https://google.com")
+# driver.get("https://google.com")
 
-search_element = driver.find_element(By.NAME, 'q')
+# search_element = driver.find_element(By.NAME, 'q')
 # search_element.send_keys("selenium" + Keys.ENTER)
 # actions.key_down(Keys.CONTROL).send_keys('a').perform()
 
 
 # actions.key_down(Keys.SHIFT).send_keys_to_element(search_element, 'selenium').perform()
-actions.key_down(Keys.SHIFT).send_keys_to_element(search_element, 'selenium').key_up(Keys.SHIFT).send_keys(" selenium").perform()
+# actions.key_down(Keys.SHIFT).send_keys_to_element(search_element, 'selenium').key_up(Keys.SHIFT).send_keys(" selenium").perform()
 
-sleep(2)
 
 # search_element.clear()
-search_element.click()
+# search_element.click()
 
-actions.key_down(Keys.CONTROL).send_keys('a').send_keys(Keys.DELETE).perform()
+# actions.key_down(Keys.CONTROL).send_keys('a').send_keys(Keys.DELETE).perform()
 
-sleep(2)
+
+# driver.get("https://play2.automationcamp.ir/")
+
+# double_click = driver.find_element(By.XPATH, "//button[text()='Double-click me']")
+
+# actions.double_click(double_click).perform()
+
+# result = driver.find_element(By.XPATH, "(//p[@id='demo' ])[1]").text
+
+# assert result == "Your Sample Double Click worked!"
+
+
+# el = driver.find_element(By.XPATH, "//*[@id='fname']")
+# actions.context_click(el).perform()
+
+# el = driver.find_element(By.XPATH, "//*[@class='tooltip']")
+# actions.move_to_element(el).perform()
+
+# driver.get("https://demos.telerik.com/kendo-ui/circular-gauge/index")
+# driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
+# driver.execute_script("scroll(0, 400)")
+
+
+
+# element_1 = driver.find_element(By.XPATH, "//*[contains(@class, 'k-button-increase')]")
+# element_2 = driver.find_element(By.XPATH, "//*[contains(@class, 'k-button-decrease')]")
+
+# actions.click_and_hold(element).perform()
+# actions.click_and_hold(element_1).pause(3).release().click_and_hold(element_2).pause(3).release().perform()
+
+
+driver.get("https://play2.automationcamp.ir/")
+
+# element_1 = driver.find_element(By.ID, "drag1")
+# element_2 = driver.find_element(By.ID, "div1")
+
+
+# actions.move_to_element(element_1).click_and_hold().move_to_element(element_2).release().perform()
+
+# actions.drag_and_drop(element_1, element_2).perform()
+
 # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+driver.execute_script("scroll(0, 500)")
 
+# driver.maximize_window()
+
+# offset = driver.find_element(By.XPATH, "//*[text()='Lets you select only one option']").location
+
+# print(offset)
+
+# driver.find_element(By.ID, "option").click()
+
+# actions.move_by_offset(offset['x'], offset['y']).click().perform()
+
+
+element_1 = driver.find_element(By.ID, "drag1")
+element_1_offset = element_1.location
+element_2_offset = driver.find_element(By.ID, "div1").location
+offset = {}
+offset['x'] = element_2_offset['x'] - element_1_offset['x']
+offset['y'] = element_2_offset['y'] - element_1_offset['y']
+actions.drag_and_drop_by_offset(element_1, offset['x'], offset['y']).perform()
+
+print(offset)
+
+sleep(2)
 
 # sleep(10)
 # current_path = Path(__file__).parent
